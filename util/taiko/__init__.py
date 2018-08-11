62062231d88d07a9a9cb7fe8ec4546e22b58b19d
howeverforever <longbiau@hotmail.com.tw>
2018-08-11 13:29:08 +0800

    Add EP cache.

--- util/taiko/__init__.py
+++ util/taiko/__init__.py
@@ -2,5 +2,6 @@ from .config import *
 from .io import *
 from .preprocessing import *
 from .image import *
+from .cache import *
 
 __version__ = '0.3'
---
+++ util/taiko/cache.py
@@ -0,0 +1,27 @@
+from .preprocessing.performance import *
+from .io.note import *
+
+import pandas as pd
+
+
+__all__ = ['get_event_primitive_df']
+
+
+def get_event_primitive_df(who_id, song_id, order_id, scaling=True, resampling=True):
+    boolean_dict = {True: 'y', False: 'n'}
+    resampling_boolean = {True: '0.02S', False: None}
+
+    filename = '%d-%d-%d-%s%s' % (who_id, song_id, order_id, boolean_dict[scaling], boolean_dict[resampling])
+
+    try:
+        event_primitive_df = pd.read_csv('CSV/event_primitive/' + filename + '.csv')
+    except FileNotFoundError:
+        event_primitive_df = get_performance(who_id,
+                                             song_id,
+                                             order_id,
+                                             scaling,
+                                             resampling_boolean[resampling]).event_primitive_df
+        filename = '%d-%d-%d-%s%s' % (who_id, song_id, order_id, boolean_dict[scaling], boolean_dict[resampling])
+        event_primitive_df.to_csv('CSV/event_primitive/' + filename + '.csv', index=False)
+
+    return event_primitive_df
