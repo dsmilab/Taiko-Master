21b48af25a0369da4bba3e5ba5731fe750067615
howeverforever <longbiau@hotmail.com.tw>
2018-08-24 14:57:28 +0800

    Transform custom hit_type.

--- util/taiko/cache.py
+++ util/taiko/cache.py
@@ -6,6 +6,7 @@ import re
 ACC_REGEX = '^\w*_A[XYZ]{0,2}_\w*$'
 GYR_REGEX = '^\w*_G[XYZ]{0,2}_\w*$'
 NEAR_REGEX = '^[LR]\d+$'
+HIT_TYPE_REGEX = '^(hit_type|[A-Z]+\d)$'
 
 __all__ = ['get_event_primitive_df']
 
@@ -42,4 +43,74 @@ def get_event_primitive_df(who_id, song_id, order_id, scaling=True, resampling=T
         columns = [col for col in event_primitive_df.columns if re.match(NEAR_REGEX, col)]
         event_primitive_df.drop(columns, axis=1, inplace=True)
 
+    columns = event_primitive_df.columns
+    columns = [col for col in columns if re.match(HIT_TYPE_REGEX, col)]
+    for col in columns:
+        event_primitive_df.loc[:, col] = event_primitive_df[col].apply(transform_hit_type_label_dong_ka)
     return event_primitive_df
+
+
+def transform_hit_type_label(label):
+    """
+    Relabel the column.
+
+    :param label: original label
+    :return: transformed label
+    """
+
+    if label in [1, 2, 3, 4]:
+        return 1
+    elif label in [5, 6, 7]:
+        return 2
+    return 0
+
+
+def transform_hit_type_label_dong_ka(label):
+    """
+    Relabel the column.
+
+    :param label: original label
+    :return: transformed label
+    """
+
+    if label in [1, 3]:
+        return 1
+    elif label in [2, 4]:
+        return 2
+    elif label in [5, 6, 7]:
+        return 3
+    return 0
+
+
+def transform_hit_type_label_ship(label):
+    """
+    Relabel the column.
+
+    :param label: original label
+    :return: transformed label
+    """
+
+    if label in [1, 2, 3, 4]:
+        return 1
+    elif label in [5, 6]:
+        return 2
+    elif label in [7]:
+        return 3
+    return 0
+
+
+def transform_hit_type_label_big_small(label):
+    """
+    Relabel the column.
+
+    :param label: original label
+    :return: transformed label
+    """
+
+    if label in [1, 2]:
+        return 1
+    elif label in [3, 4]:
+        return 2
+    elif label in [5, 6, 7]:
+        return 3
+    return 0
