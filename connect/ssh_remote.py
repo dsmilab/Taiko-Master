e2248067f6724f419bc258e5b65e501a64ef8c59
howeverforever <longbiau@hotmail.com.tw>
2018-09-15 12:35:37 +0800

    Rearrange files.

--- connect/ssh_remote.py
+++ connect/ssh_remote.py
@@ -1,32 +1,53 @@
-#   1. Install the paramikio module for python.
-#       pip install paramiko
-#   2. Edit the SSH details below.
-
+import argparse
 import paramiko
+import threading
 import sys
 
-## EDIT SSH DETAILS ##
-
-SSH_ADDRESS = "10.0.1.44"
+SSH_ADDRESS_1 = "10.0.1.39"
+SSH_ADDRESS_2 = "10.0.1.44"
 SSH_USERNAME = "debian"
-SSH_PASSWORD = "beaglebone"
-SSH_COMMAND = "cd Projects/beagle; python read10axis.py -6;"
+SSH_RECORD_COMMAND = "cd Projects/beagle; python read10axis.py -6;"
+SSH_KILL_COMMAND = "pkill -f python;"
 
-## CODE BELOW ##
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+def work(host_ip, command):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh_stdin = ssh_stdout = ssh_stderr = None
+    with open('data/passwd.tk', 'r') as f:
+        pwd = f.readline()[:-1]
 
-try:
-    ssh.connect(SSH_ADDRESS, username=SSH_USERNAME, password=SSH_PASSWORD)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(SSH_COMMAND)
-except Exception as e:
-    sys.stderr.write("SSH connection error: {0}".format(e))
+        try:
+            ssh.connect(host_ip, username=SSH_USERNAME, password=pwd)
+            ssh.exec_command(command)
+            print('connect %s ok' % host_ip)
 
-if ssh_stdout:
-    print(ssh_stdout.read())
-    sys.stdout.write(ssh_stdout.read())
-if ssh_stderr:
-    sys.stderr.write(ssh_stderr.read())
+        except Exception as e:
+            sys.stderr.write("SSH connection error: {0}".format(e))
+            sys.stderr.flush()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Collect data from BeagleBone Blue')
+    parser.add_argument('-kill', help='stop record', action='store_true')
+
+    args = vars(parser.parse_args())
+    is_kill = args['kill']
+
+    hosts = [SSH_ADDRESS_1, SSH_ADDRESS_2]
+    threads = []
+    for h in hosts:
+        command = SSH_RECORD_COMMAND
+        if is_kill:
+            command = SSH_KILL_COMMAND
+
+        t = threading.Thread(target=work, args=(h, command,))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+
+
+if __name__ == '__main__':
+    main()
